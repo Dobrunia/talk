14beeac30855cc964054a21e465e6008d445575e
Dobrunia Kostrigin <48620984+Dobrunia@users.noreply.github.com>
2024-11-01 13:21:42 +0300

    dev

--- GigaTalk/src/api/serverApi.ts
+++ GigaTalk/src/api/serverApi.ts
@@ -1,7 +1,6 @@
 import apiClient from './axiosInstance.ts';
 
 export const serverApi = {
-  // Получить список всех серверов
   async getAllServers() {
     const response = await apiClient.get('/getAllServers');
     return response.data;
--- GigaTalk/src/main.ts
+++ GigaTalk/src/main.ts
@@ -2,38 +2,6 @@ import './ui-kit/style.css';
 import { renderProfile } from './ui-kit';
 import { loadServers, serverClickHandler } from './utils/cache';
 
-// const DATA: serverDATA[] = [
-//   {
-//     id: 1,
-//     imageUrl: '/1.gif',
-//     name: 'Название Сервера 1',
-//     categories: [
-//       {
-//         id: 1,
-//         name: 'первая категория',
-//         channels: [
-//           {
-//             id: 1,
-//             name: 'первый войс канал',
-//             type: 'voice',
-//           },
-//         ],
-//       },
-//       {
-//         id: 2,
-//         name: 'вторая категория',
-//         channels: [
-//           {
-//             id: 2,
-//             name: '2 войс канал',
-//             type: 'voice',
-//           },
-//         ],
-//       },
-//     ],
-//   },
-// ];
-
 async function start() {
   localStorage.setItem('username', 'Dobrunia');
   loadServers();
