3ddab4c72fe953183899fc06f57475e5b5903812
Dobrunia Kostrigin <48620984+Dobrunia@users.noreply.github.com>
2024-11-01 14:22:07 +0300

    authModal

--- GigaTalk/src/global.d.ts
+++ GigaTalk/src/global.d.ts
@@ -3,5 +3,8 @@ export {};
 declare global {
   interface Window {
     serverClickHandler: typeof serverClickHandler;
+    showLogin: typeof showLogin;
+    showRegister: typeof showRegister;
+    closeAuthModal: typeof closeAuthModal;
   }
 }
