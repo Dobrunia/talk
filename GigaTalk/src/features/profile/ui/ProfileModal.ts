fb16c6567a6ce40d641fc0eaf2854d1eab0e924f
Dobrunia Kostrigin <48620984+Dobrunia@users.noreply.github.com>
2024-11-27 16:51:51 +0300

    dev

--- GigaTalk/src/features/profile/ui/ProfileModal.ts
+++ GigaTalk/src/features/profile/ui/ProfileModal.ts
@@ -10,17 +10,20 @@ export function renderProfileModal() {
 
   const header = document.createElement('h2');
   header.textContent = 'Поменять аватарку';
-  header.className = 'header';
+  header.className = 'modal-header';
+
   modalContent.appendChild(header);
 
   // Форма для изменения аватара
   const avatarForm = document.createElement('form');
   avatarForm.id = 'avatarForm';
-  avatarForm.className = 'form';
+  avatarForm.className = 'modal-form';
   avatarForm.onsubmit = (event) => handleAvatarChange(event);
 
   // Поле для загрузки аватара
   const fileInputContainer = document.createElement('div');
+  fileInputContainer.className = 'modal-container';
+
   const fileInputLabel = document.createElement('label');
   fileInputLabel.htmlFor = 'change_avatar';
   fileInputLabel.textContent = 'Новый аватар';
@@ -31,24 +34,27 @@ export function renderProfileModal() {
   fileInput.type = 'file';
   fileInput.name = 'change_avatar';
   fileInput.accept = 'image/*';
-  fileInput.className = 'input input-file';
+  fileInput.className = 'modal-input-file';
+
+  fileInputContainer.appendChild(fileInput);
+  avatarForm.appendChild(fileInputContainer);
 
   // Предварительный просмотр изображения
   const previewContainer = document.createElement('div');
   const previewLabel = document.createElement('label');
   previewLabel.textContent = 'Предварительный просмотр:';
-  previewLabel.className = 'modal-label';
 
   const previewImageWrapper = document.createElement('div');
+  previewImageWrapper.className = 'preview-container';
+
   const previewImage = document.createElement('img');
-  previewImage.className = 'preview-image';
   previewImage.alt = 'Предварительный просмотр';
+  previewImage.className = 'preview-image hidden';
 
   fileInput.addEventListener('change', (event) => previewImageHandler(event, previewImage));
-  fileInputContainer.appendChild(fileInput);
-  avatarForm.appendChild(fileInputContainer);
-  previewContainer.appendChild(previewLabel);
   previewImageWrapper.appendChild(previewImage);
+
+  previewContainer.appendChild(previewLabel);
   previewContainer.appendChild(previewImageWrapper);
   avatarForm.appendChild(previewContainer);
 
@@ -57,6 +63,7 @@ export function renderProfileModal() {
   saveButton.type = 'submit';
   saveButton.textContent = 'Сохранить аватар';
   saveButton.className = 'button button-primary';
+
   avatarForm.appendChild(saveButton);
 
   modalContent.appendChild(avatarForm);
